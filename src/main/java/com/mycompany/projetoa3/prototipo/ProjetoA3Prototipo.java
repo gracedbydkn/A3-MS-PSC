e98e535b4d88c5c2b2ed6f2977373e597b363e55
gutinho <84037699+gracedbydkn@users.noreply.github.com>
2024-10-29 15:26:56 -0300

    Adição das telas do projeto
    Telas do projeto, desde login até o cadastro e gerenciamento.
    
    Telas "Menu" e "Gerenciar grupos" não estão feitas.

---
+++ src/main/java/com/mycompany/projetoa3/prototipo/ProjetoA3Prototipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa3.prototipo;
+
+/**
+ *
+ * @author 824144932
+ */
+public class ProjetoA3Prototipo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
